5d410f0d5d0c6f874d40e31f64d0c8e8e1b9b135
wangkuakua <wangkaukua@qq.com>
2019-04-02 19:51:30 +0800

    修改一次

---
+++ gitTest-project/src/w/wkk/git/FirstClass.java
@@ -0,0 +1,12 @@
+package w.wkk.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello,Git");
+        System.out.println("hello,Git");
+        System.out.println("hello,Git");
+        System.out.println("hello,Git");
+    }
+
+
+}
